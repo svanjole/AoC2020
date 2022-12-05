cc24e20cc5148d0d69fc665502752cc82934b92d
Stefan van Jole <stefan.van.jole@rabobank.com>
2022-12-05 13:05:02 +0100

    AoC 2022 initial commit! Contains 2021 as well :)

--- solutions/2022/day04.py
+++ solutions/2022/day04.py
@@ -6,14 +6,11 @@ class Day04:
     def is_contained_by(self, a, b):
         return self.check(a, b) or self.check(b, a)
 
-    def base_solve(self, input_data):
+    def solve(self, input_data):
         count = 0
         for line in input_data.split("\n"):
-            sections = list(map(lambda x:  pytx.split("-"), line.split(",")))
-            if self.is_contained_by(
-                    list(map(int, sections[0])),
-                    list(map(int, sections[1]))
-            ):
+            sections = [[int(y) for y in x.split("-")] for x in line.split(",")]
+            if self.is_contained_by(sections[0], sections[1]):
                 count += 1
 
         return count
@@ -28,14 +25,8 @@ class Day04PartA(Day04, FileReaderSolution):
     def check(a, b):
         return a[0] <= b[0] and a[1] >= b[1]
 
-    def solve(self, input_data: str) -> int:
-        return self.base_solve(input_data)
-
 
 class Day04PartB(Day04, FileReaderSolution):
     @staticmethod
     def check(a, b):
         return a[0] <= b[0] <= a[1] or a[0] <= b[1] <= a[1]
-
-    def solve(self, input_data: str) -> int:
-        return self.base_solve(input_data)
