7e15a285a60b0d7e50a28e0c64df89a70ef60aa0
Stefan van Jole <stefan.van.jole@rabobank.com>
2023-12-04 16:06:38 +0100

    Small refactoring

--- solutions/2023/day04.py
+++ solutions/2023/day04.py
@@ -4,35 +4,35 @@ from utils.abstract import FileReaderSolution
 class Day04:
     @staticmethod
     def parse(input_data):
-        cards = []
+        matches = []
 
         for line in input_data.splitlines():
             numbers = line.split(": ")[1].split(" | ")
 
-            winners = [int(x.strip(" ")) for x in numbers[0].split(" ") if x]
-            numbers = [int(x.strip(" ")) for x in numbers[1].split(" ") if x]
+            winners = [int(x) for x in numbers[0].split(" ") if x]
+            numbers = [int(x) for x in numbers[1].split(" ") if x]
 
-            cards.append(len(set(winners) & set(numbers)))
+            matches.append(len(set(winners) & set(numbers)))
 
-        return cards
+        return matches
 
 
 class Day04PartA(Day04, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        cards = self.parse(input_data)
+        matches = self.parse(input_data)
 
-        return sum([pow(2, matches-1) if matches > 0 else 0 for matches in cards])
+        return sum([pow(2, match-1) if match > 0 else 0 for match in matches])
 
 
 class Day04PartB(Day04, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        cards = self.parse(input_data)
-        amounts = [0] * len(cards)
+        matches = self.parse(input_data)
+        amounts = [0] * len(matches)
 
-        for key, matches in enumerate(cards):
+        for key, match in enumerate(matches):
             amounts[key] += 1
 
-            for copy in range(key+1, key+1+matches):
+            for copy in range(key+1, key+1+match):
                 amounts[copy] += amounts[key]
 
         return sum(amounts)
